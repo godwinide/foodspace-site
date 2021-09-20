37a017725aa5c9c1768282e000ddc656a659fc06
Godwin Idemudia <godwinide@gmail.com>
2021-09-20 16:37:15 +0100

    removed error on port

--- client/src/config/client.js
+++ client/src/config/client.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const client = axios.create({
-    baseURL: "http:192.168.43.97:2021/api",
+    baseURL: "http:192.168.43.97:2019/api",
     headers: {
         Accept: "application/json"
     }
