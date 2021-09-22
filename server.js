b5817b8e00b9dba7c19f3444668fa5d62ce93e00
Godwin Idemudia <godwinide@gmail.com>
2021-09-23 00:41:14 +0100

    fixing errors

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.static("./public"))
 app.use(cors())
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static("./client/build"))
 
 // URLS
 require("./urls")(app);
